4115dff2086afab9888c7d97737fa6c6912aae37
Moekr <highter@outlook.com>
2018-03-18 15:17:58 +0800

    修正一个导致空指针的问题

--- src/main/java/com/moekr/aes/web/controller/api/CallbackController.java
+++ src/main/java/com/moekr/aes/web/controller/api/CallbackController.java
@@ -3,6 +3,7 @@ package com.moekr.aes.web.controller.api;
 import com.moekr.aes.logic.service.RecordService;
 import com.moekr.aes.util.AesProperties;
 import com.moekr.aes.util.Asserts;
+import com.moekr.aes.util.ServiceException;
 import com.moekr.aes.util.ToolKit;
 import com.moekr.aes.web.dto.form.CallbackForm;
 import org.apache.commons.lang3.StringUtils;
@@ -28,7 +29,9 @@ public class CallbackController {
 
 	@PostMapping("/{id}")
 	public Map<String, Object> callback(@ModelAttribute @Valid CallbackForm form, Errors errors, @PathVariable String id) {
-		Asserts.isTrue(!errors.hasFieldErrors(), HttpStatus.SC_BAD_REQUEST, errors.getFieldError().getDefaultMessage());
+		if (errors.hasFieldErrors()) {
+			throw new ServiceException(HttpStatus.SC_BAD_REQUEST, errors.getFieldError().getDefaultMessage());
+		}
 		Asserts.isTrue(StringUtils.equals(this.secret, form.getSecret()), HttpStatus.SC_FORBIDDEN, "认证失败！");
 		Asserts.isTrue(StringUtils.isNumeric(id), HttpStatus.SC_BAD_REQUEST, "ID格式不正确！");
 		recordService.asyncRecord(Integer.valueOf(id), form.getBuildNumber());
